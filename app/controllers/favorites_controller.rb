86a678e261bf5e83cc3037e782fbaa22c51093ca
kasumi sogabe <ksm17sgb@gmail.com>
2023-04-05 00:33:41 +0900

    投稿にユーザ名表示

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,11 +2,11 @@ class FavoritesController < ApplicationController
 
   def create
     favorite = current_user.favorites.create(flower_id: params[:flower_id])
-    redirect_to flowers_path, notice: "#{favorite.flower.user_id}さんのブログをお気に入り登録しました"
+    redirect_to flowers_path, notice: "#{favorite.flower.favorite_users.name}さんのブログをお気に入り登録しました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to flowers_path, notice: "#{favorite.flower.user_id}さんのブログをお気に入り解除しました"
+    redirect_to flowers_path, notice: "#{favorite.flower.favorite_users.name}さんのブログをお気に入り解除しました"
   end
 end
