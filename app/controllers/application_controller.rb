73d5ef381430e09227bdfb1b0a8030727a2feb24
kasumi sogabe <ksm17sgb@gmail.com>
2023-04-05 00:09:56 +0900

    nameを使ってサインアップ機能追加のため

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # サインアップする際にnameの値を送信することを許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
