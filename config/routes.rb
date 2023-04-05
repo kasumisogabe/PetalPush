7f061b4d01d2b213cee35b0b877e95d688ff8c88
kasumi sogabe <ksm17sgb@gmail.com>
2023-04-05 11:08:00 +0900

    お気に入り一覧作成のためのマイページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :flowers
   resources :favorites, only: [:create, :destroy]
+  resources :users, only: [:show, :edit, :update]
   
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
