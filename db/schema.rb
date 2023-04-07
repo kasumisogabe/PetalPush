f5ba56f14fb4797056ad30673c758a25525a087a
kasumi sogabe <ksm17sgb@gmail.com>
2023-04-07 17:19:34 +0900

    コメント機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_04_144656) do
+ActiveRecord::Schema.define(version: 2023_04_07_073047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2023_04_04_144656) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "flower_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["flower_id"], name: "index_comments_on_flower_id"
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -90,6 +98,7 @@ ActiveRecord::Schema.define(version: 2023_04_04_144656) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "flowers"
   add_foreign_key "favorites", "flowers"
   add_foreign_key "favorites", "users"
   add_foreign_key "flowers", "users"
