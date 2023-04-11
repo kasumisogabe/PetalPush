0220662f28f78c8ba891a302e7c0ed80aa57d869
kasumi sogabe <ksm17sgb@gmail.com>
2023-04-12 01:10:43 +0900

    プロフィール画像実装途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # サインアップする際にnameの値を送信することを許可
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :avatar] )
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :avatar])
   end
 end
