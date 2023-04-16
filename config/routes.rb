19337cda8480f7f4334a3c26ed05a6fada2181ec
kasumi sogabe <ksm17sgb@gmail.com>
2023-04-16 20:28:27 +0900

    トップページの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root 'flowers#index'
+  root 'tops#index'
 
   devise_for :users
   resources :flowers do
