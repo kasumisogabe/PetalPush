1afacbae651bd95cea4d71ff22548bfcaf331125
kasumi sogabe <ksm17sgb@gmail.com>
2023-04-18 15:45:01 +0900

    作ったテストが全て通るか確認するため

--- spec/models/flower_spec.rb
+++ spec/models/flower_spec.rb
@@ -2,36 +2,36 @@ require 'rails_helper'
 RSpec.describe Flower, type: :model do
   let(:user) { User.new(name: 'テストユーザー', email: 'test@example.com', password: 'password', password_confirmation: 'password') }
 
-  # describe 'バリデーションのテスト' do
-  #   context 'nameカラム' do
-  #     it 'nameが空の場合、バリデーションにひっかかる' do
-  #       flower = Flower.new(name: '', address: '東京タワー', user: user)
-  #       expect(flower).not_to be_valid
-  #     end
+  describe 'バリデーションのテスト' do
+    context 'nameカラム' do
+      it 'nameが空の場合、バリデーションにひっかかる' do
+        flower = Flower.new(name: '', address: '東京タワー', user: user)
+        expect(flower).not_to be_valid
+      end
 
-  #     it 'nameが301文字以上の場合、バリデーションにひっかかる' do
-  #       flower = Flower.new(name: 'a' * 301, address: '東京タワー', user: user)
-  #       expect(flower).not_to be_valid
-  #     end
+      it 'nameが301文字以上の場合、バリデーションにひっかかる' do
+        flower = Flower.new(name: 'a' * 301, address: '東京タワー', user: user)
+        expect(flower).not_to be_valid
+      end
 
-  #     it 'nameが300文字以内の場合、バリデーションが通る' do
-  #       flower = Flower.new(name: 'a' * 300, address: '東京タワー', user: user)
-  #       expect(flower).to be_valid
-  #     end
-  #   end
+      it 'nameが300文字以内の場合、バリデーションが通る' do
+        flower = Flower.new(name: 'a' * 300, address: '東京タワー', user: user)
+        expect(flower).to be_valid
+      end
+    end
 
-  #   context 'addresカラム' do
-  #     it 'addressが空の場合、バリデーションにひっかかる' do
-  #       flower = Flower.new(name: '桜', address: '', user: user)
-  #       expect(flower).not_to be_valid
-  #     end
+    context 'addresカラム' do
+      it 'addressが空の場合、バリデーションにひっかかる' do
+        flower = Flower.new(name: '桜', address: '', user: user)
+        expect(flower).not_to be_valid
+      end
 
-  #     it 'addressが存在する場合、バリデーションが通る' do
-  #       flower = Flower.new(name: '桜', address: '東京タワー', user: user)
-  #       expect(flower).to be_valid
-  #     end
-  #   end
-  # end
+      it 'addressが存在する場合、バリデーションが通る' do
+        flower = Flower.new(name: '桜', address: '東京タワー', user: user)
+        expect(flower).to be_valid
+      end
+    end
+  end
 
   describe 'ransackメソッドのテスト' do
     it '検索可能なカラムが正しいこと' do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,70 +1,70 @@
 require 'rails_helper'
 RSpec.describe 'Userモデル機能', type: :model do
   
-  # describe 'バリデーションのテスト' do
-  #   context '名前が空の場合' do
-  #     it 'バリデーションにひっかかる' do
-  #       user = User.new(name: '')
-  #       expect(user).not_to be_valid
-  #     end
-  #   end
+  describe 'バリデーションのテスト' do
+    context '名前が空の場合' do
+      it 'バリデーションにひっかかる' do
+        user = User.new(name: '')
+        expect(user).not_to be_valid
+      end
+    end
   
-  #   context '名前が301文字以上の場合' do
-  #     it 'バリデーションにひっかかる' do
-  #       user = User.new(name: 'a' * 301)
-  #       expect(user).not_to be_valid
-  #     end
-  #   end
+    context '名前が301文字以上の場合' do
+      it 'バリデーションにひっかかる' do
+        user = User.new(name: 'a' * 301)
+        expect(user).not_to be_valid
+      end
+    end
   
-  #   context 'メールアドレスが空の場合' do
-  #     it 'バリデーションにひっかかる' do
-  #       user = User.new(email: '')
-  #       expect(user).not_to be_valid
-  #     end
-  #   end
+    context 'メールアドレスが空の場合' do
+      it 'バリデーションにひっかかる' do
+        user = User.new(email: '')
+        expect(user).not_to be_valid
+      end
+    end
   
-  #   context 'メールアドレスが301文字以上の場合' do
-  #     it 'バリデーションにひっかかる' do
-  #       user = User.new(email: 'a' * 244 + '@example.com')
-  #       expect(user).not_to be_valid
-  #     end
-  #   end
+    context 'メールアドレスが301文字以上の場合' do
+      it 'バリデーションにひっかかる' do
+        user = User.new(email: 'a' * 244 + '@example.com')
+        expect(user).not_to be_valid
+      end
+    end
   
-  #   context 'パスワードが5文字以下の場合' do
-  #     it 'バリデーションにひっかかる' do
-  #       user = User.new(password: '12345', password_confirmation: '12345')
-  #       expect(user).not_to be_valid
-  #     end
-  #   end
+    context 'パスワードが5文字以下の場合' do
+      it 'バリデーションにひっかかる' do
+        user = User.new(password: '12345', password_confirmation: '12345')
+        expect(user).not_to be_valid
+      end
+    end
   
-  #   context 'パスワードが301文字以上の場合' do
-  #     it 'バリデーションにひっかかる' do
-  #       user = User.new(password: 'a' * 301, password_confirmation: 'a' * 301)
-  #       expect(user).not_to be_valid
-  #     end
-  #   end
-  # end
+    context 'パスワードが301文字以上の場合' do
+      it 'バリデーションにひっかかる' do
+        user = User.new(password: 'a' * 301, password_confirmation: 'a' * 301)
+        expect(user).not_to be_valid
+      end
+    end
+  end
 
-  # describe 'ゲストユーザーのテスト' do
-  #   context '一般のゲストユーザーを作成する場合' do
-  #     it 'ゲストユーザーが正しく作成されること' do
-  #       guest = User.guest
-  #       expect(guest.email).to eq 'guest@example.com'
-  #       expect(guest.name).to eq 'ゲスト'
-  #       expect(guest).to be_persisted
-  #     end
-  #   end
+  describe 'ゲストユーザーのテスト' do
+    context '一般のゲストユーザーを作成する場合' do
+      it 'ゲストユーザーが正しく作成されること' do
+        guest = User.guest
+        expect(guest.email).to eq 'guest@example.com'
+        expect(guest.name).to eq 'ゲスト'
+        expect(guest).to be_persisted
+      end
+    end
   
-  #   context '管理者のゲストユーザーを作成する場合' do
-  #     it 'ゲスト管理者ユーザーが正しく作成されること' do
-  #       guest_admin = User.guest_admin
-  #       expect(guest_admin.email).to eq 'guest_admin@example.com'
-  #       expect(guest_admin.name).to eq 'ゲスト管理者'
-  #       expect(guest_admin.admin).to be true
-  #       expect(guest_admin).to be_persisted
-  #     end
-  #   end
-  # end
+    context '管理者のゲストユーザーを作成する場合' do
+      it 'ゲスト管理者ユーザーが正しく作成されること' do
+        guest_admin = User.guest_admin
+        expect(guest_admin.email).to eq 'guest_admin@example.com'
+        expect(guest_admin.name).to eq 'ゲスト管理者'
+        expect(guest_admin.admin).to be true
+        expect(guest_admin).to be_persisted
+      end
+    end
+  end
 
   describe 'パスワード要件のテスト' do
     context '新規ユーザーの場合' do
