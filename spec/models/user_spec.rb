874430ab9df21286d5129500a2502d6780a7d190
kasumi sogabe <ksm17sgb@gmail.com>
2023-04-18 11:27:30 +0900

    Userモデル内のメソッドに対してテスト実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,23 +44,50 @@ RSpec.describe 'Userモデル機能', type: :model do
   #   end
   # end
 
-  describe 'ゲストユーザーのテスト' do
-    context '一般のゲストユーザーを作成する場合' do
-      it 'ゲストユーザーが正しく作成されること' do
-        guest = User.guest
-        expect(guest.email).to eq 'guest@example.com'
-        expect(guest.name).to eq 'ゲスト'
-        expect(guest).to be_persisted
+  # describe 'ゲストユーザーのテスト' do
+  #   context '一般のゲストユーザーを作成する場合' do
+  #     it 'ゲストユーザーが正しく作成されること' do
+  #       guest = User.guest
+  #       expect(guest.email).to eq 'guest@example.com'
+  #       expect(guest.name).to eq 'ゲスト'
+  #       expect(guest).to be_persisted
+  #     end
+  #   end
+  
+  #   context '管理者のゲストユーザーを作成する場合' do
+  #     it 'ゲスト管理者ユーザーが正しく作成されること' do
+  #       guest_admin = User.guest_admin
+  #       expect(guest_admin.email).to eq 'guest_admin@example.com'
+  #       expect(guest_admin.name).to eq 'ゲスト管理者'
+  #       expect(guest_admin.admin).to be true
+  #       expect(guest_admin).to be_persisted
+  #     end
+  #   end
+  # end
+
+  describe 'パスワード要件のテスト' do
+    context '新規ユーザーの場合' do
+      it 'パスワードが必須であること' do
+        user = User.new(name: 'Test User', email: 'test@example.com')
+        expect(user.send(:password_required?)).to be true
       end
     end
   
-    context '管理者のゲストユーザーを作成する場合' do
-      it 'ゲスト管理者ユーザーが正しく作成されること' do
-        guest_admin = User.guest_admin
-        expect(guest_admin.email).to eq 'guest_admin@example.com'
-        expect(guest_admin.name).to eq 'ゲスト管理者'
-        expect(guest_admin.admin).to be true
-        expect(guest_admin).to be_persisted
+    context '既存ユーザーがパスワードを変更しない場合' do
+      it 'パスワードが必須でないこと' do
+        user = User.create(name: 'Test User', email: 'test@example.com', password: 'password', password_confirmation: 'password')
+        user.name = 'Updated User'
+        user.password = ''
+        user.password_confirmation = ''
+        expect(user.send(:password_required?)).to be false
+      end
+    end
+  
+    context '既存ユーザーがパスワードを変更する場合' do
+      it 'パスワードが必須であること' do
+        user = User.create(name: 'Test User', email: 'test@example.com', password: 'password', password_confirmation: 'password')
+        user.password = 'new_password'
+        expect(user.send(:password_required?)).to be true
       end
     end
   end
