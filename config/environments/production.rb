ce29c2b841618412f3a1e72f3da134fb95e019e7
kasumi sogabe <ksm17sgb@gmail.com>
2023-04-19 16:52:06 +0900

    パスワード変更時のメール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,7 +122,7 @@ Rails.application.configure do
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.smtp_settings = {
-  address: 'ksm17sgb.gmail.com',
+  address: 'smtp.gmail.com',
   port: 587,
   domain: 'immense-ocean-81854.herokuapp.com',
   authentication: :plain,
