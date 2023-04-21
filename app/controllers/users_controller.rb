ff0f7326a821c0a4d9023ed05d50c65a72e9818b
kasumi sogabe <ksm17sgb@gmail.com>
2023-04-21 11:01:47 +0900

    マイページの画像表示順を最新順に修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @flowers = @user.flowers
+    @flowers = @user.flowers.order("created_at desc")
   end
 
   def edit
